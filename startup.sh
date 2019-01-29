c8ffd1732aafb0f9faa9afdd3c2fe2f54b078c35
Patrick <31744877+patricklee2@users.noreply.github.com>
2019-01-28 16:52:27 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Get environment variables to show up in SSH session
-eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
-
 service ssh start
 sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
 mkdir /var/lock/apache2

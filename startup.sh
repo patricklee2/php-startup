f84b606a2a3949477192ac676caf1c3de8a1204b
Patrick <31744877+patricklee2@users.noreply.github.com>
2019-01-28 16:44:38 -0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-cat >/etc/motd <<EOL 
-  _____                               
-  /  _  \ __________ _________   ____  
- /  /_\  \\___   /  |  \_  __ \_/ __ \ 
-/    |    \/    /|  |  /|  | \/\  ___/ 
-\____|__  /_____ \____/ |__|    \___  >
-        \/      \/                  \/ 
-A P P   S E R V I C E   O N   L I N U X
-
-Documentation: http://aka.ms/webapp-linux
-PHP quickstart: https://aka.ms/php-qs
-
-EOL
-cat /etc/motd
 
 # Get environment variables to show up in SSH session
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
